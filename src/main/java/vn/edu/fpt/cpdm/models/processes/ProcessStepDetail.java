d593841599f7c04915e958af8b51f8360e0fc9d6
NhanNHB <NhanNHB92@gmail.com>
2019-06-04 17:24:40 +0700

    update Step Detail model

--- src/main/java/vn/edu/fpt/cpdm/models/processes/ProcessStepDetail.java
+++ src/main/java/vn/edu/fpt/cpdm/models/processes/ProcessStepDetail.java
@@ -3,5 +3,6 @@ package vn.edu.fpt.cpdm.models.processes;
 import java.util.List;
 
 public interface ProcessStepDetail extends ProcessStepSummary {
+    String getDescription();
     List<StepOutcomeDetail> getOutcomes();
 }
